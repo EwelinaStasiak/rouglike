faff230ffb8aca5699c690395ba52da7dc20fe58
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2020-11-25 11:13:38 +0100

    Improve movement player function

--- movement.py
+++ movement.py
@@ -1,35 +1,33 @@
-import util
-
-
-def player_position(player_icon, board):
+def character_position(character_icon, board):
     for row in range(len(board)):
         for col in range(len(board)):
-            if board[row][col] == player_icon:
-                player_coordinate = [row, col]
-    return player_coordinate
+            if board[row][col] == character_icon:
+                character_coordinate = [row, col]
+    return character_coordinate
 
 
-def player_move(key, board, player_icon):
-
-    player_coordinate = player_position(player_icon, board)
-    player_row = player_coordinate[0]
-    player_col = player_coordinate[1]
-
+def direction_of_movement(key, character_coordinate):
+    row = character_coordinate[0]
+    col = character_coordinate[1]
+    
     if key.upper() == "W":
-        if board[player_row - 1][player_col] != "#":
-            board[player_row][player_col] = " "
-            board[player_row - 1][player_col] = "@"
+        return [row - 1, col]
     elif key.upper() == "S":
-        if board[player_row + 1][player_col] != "#":
-            board[player_row][player_col] = " "
-            board[player_row + 1][player_col] = "@"
+        return [row + 1, col]
     elif key.upper() == "A":
-        if board[player_row][player_col - 1] != "#":
-            board[player_row][player_col] = " "
-            board[player_row][player_col - 1] = "@"
+        return [row, col - 1]
     elif key.upper() == "D":
-        if board[player_row][player_col + 1] != "#":
-            board[player_row][player_col] = " "
-            board[player_row][player_col + 1] = "@"
+        return [row, col + 1]
+    else:
+        return [row, col]
 
+
+def player_move(board, player_icon, key):
+
+    player_coordinate = character_position(player_icon, board)
+    next_player_coordinate = direction_of_movement(key, player_coordinate)
+
+    if board[next_player_coordinate[0]][next_player_coordinate[1]] == " " or board[next_player_coordinate[0]][next_player_coordinate[1]] == player_icon:
+        board[player_coordinate[0]][player_coordinate[1]] = " "
+        board[next_player_coordinate[0]][next_player_coordinate[1]] = player_icon
     return board
