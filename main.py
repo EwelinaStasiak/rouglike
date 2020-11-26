473c2114238f28ef37d0624b2b4361f45c24d43b
Wojciech Dziadoń <wdziadon@intercars.eu>
2020-11-26 11:40:24 +0100

    creat hero and items added

--- main.py
+++ main.py
@@ -25,19 +25,33 @@ board = [["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
         ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]]
 
 
-def create_player():
-    '''
-    Creates a 'player' dictionary for storing all player related informations - i.e. player icon, player position.
-    Fell free to extend this dictionary!
+def create_player(user_choice):
+    'Tworzy bohatera wg wyboru użytkownika'
+    if user_choice == "1":
+        hero = {'name' : "Mikolaj", 'species' : "hedgehog", 'health' : 10, 'max_load' : 15}
+    elif user_choice == "2":
+        hero = {'name' : "Mikolaj", 'species' : " Magic hedgehog", 'health' : 15, 'max_load' : 10, 'skill' : "magic"}
+    elif user_choice == "3":
+        hero = {'name' : "Mikolaj", 'species' : "Invisible hedgehog", 'health' : 5, 'max_load' : 5, 'skill' : "invisible"}
+    return hero
 
-    Returns:
-    dictionary
-    '''
-    pass
-
+def create_items():
+    apple = {'name' : "Apple", 'kind' : "Food", 'value_health' : 2, 'worm': False, 'collecting' : True, 'duration' : 60, 'picture' : 127822} # collecting - czy przedmiot podnosi się autoamtycznie, czy użytkownik musi wyrazić zgodę
+    wormy_apple = {'name' : "Wormy Apple", 'kind' : "Food", 'value_healt' : 2, 'worm': True, 'collecting' : True, 'duration' :60, 'picture' : 127822}
+    egg = {'name' : "Egg", 'kind' : "Food", 'value_healt' : 5, 'collecting' : True, 'druration' : 5, 'picture' : "E"}
+    cone = {'name' : "Cone", 'kind' : "Weapon", 'weight' : 1, 'power' : 1, 'collecting' : False, 'picture' : "C"}
+    stick = {'name' :"stick", 'kind' : "Tool", 'weight' : 2, 'collecting' : False, 'picture' : "S"}
+    key = {'name' : "Key", 'kind' :"Tool", 'weight' : 1, 'pictures' : "K"}
+    worm = {'name' : "Worm", 'kind' : "Enemy", 'health' : 2, 'picture' : "W"}
+    hen = {'name' : "Hen", 'kind' : "Firend", 'health' : 5, 'picture' : "H"}
+    return apple,wormy_apple,egg,cone,stick,key,worm,hen
 
 def main():
-    #player = create_player()
+    hero = create_player("1")
+    apple,wormy_apple,egg,cone,stick,key,worm,hen = create_items()
+    
+
+
     #board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
     print(board)
 
